9679633881223173f2fa3f3857967ab2f0dd6135
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2017-10-27 16:22:50 -0400

    component creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-  <h2>{{carPart.name}}</h2>
-  <p>{{carPart.description}}</p>
-  <p>{{carPart.inStock}} in Stock</p>`
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
+  emoji = ['🎉', '😍', '😜', '👍'];
   title = 'Ultra Racing';
   carPart = {
     'id': 1,
