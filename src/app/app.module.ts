c9f3ac76a358ceb0659af3c7ce78f9822e159f3b
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2017-10-28 10:56:50 -0400

    photo blog initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import {EntryListComponent, EntryComponent} from './entries';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    EntryComponent,
+    EntryListComponent
+  ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
