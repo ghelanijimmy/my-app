efcc87d1b06570fd3165912013f7122ae96e226d
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2017-10-28 14:00:06 -0400

    comments and like button

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,4 +10,15 @@ export class EntryComponent{
   title: string = 'My First Photo';
   photo: string = 'http://placehold.it/800x500?text=Angular Basics';
   description: string = 'A Description of My First Photo';
+  comments: any[] = [{
+      name: "John",
+      comment: "A Comment"
+  },
+  {
+      name: "James",
+      comment: "Another Comment"
+  },{
+      name: "Jimmy",
+      comment: "Also a Comment"
+  }];
 }
